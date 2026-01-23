ae7e97bfa189546febcfacf6e12edb951e4054be
lgh02 <xzvxv3@gmail.com>
2026-01-23 20:13:09 +0900

    init

---
+++ src/main/java/com/project/hobby/HobbyApplication.java
@@ -0,0 +1,13 @@
+package com.project.hobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hobby/HobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
