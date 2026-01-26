93dcf055c2e47b1a710fcc006a7d99485b7a852e
chadchan <112686545+coconutcococode@users.noreply.github.com>
2026-01-26 20:21:20 +0900

    layout.js 주석 추가

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
   description: "hobby-board",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) { // 이 레이아웃을 사용하는 모든 페이지의 실제 콘텐츠가 들어갈 자리
   return (
     <html lang="en">
       <body>
