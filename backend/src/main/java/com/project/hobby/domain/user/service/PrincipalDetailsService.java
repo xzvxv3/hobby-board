7d2ad802c2fceb3ecadf242ffe9e2d1cd07c6698
lgh02 <xzvxv3@gmail.com>
2026-02-13 15:06:19 +0900

    [Feature] 스프링 시큐리티 인증을 위한 사용자 조회 및 세부 정보 클래스 구현

---
+++ backend/src/main/java/com/project/hobby/domain/user/service/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.project.hobby.domain.user.service;
+
+import com.project.hobby.domain.user.details.PrincipalDetails;
+import com.project.hobby.domain.user.entity.User;
+import com.project.hobby.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 시큐리티가 로그인을 진행할 때, 아이디(username)가 DB에 있는지 확인하는 메서드
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
+
+        // 찾은 유저 정보를 시큐리티 전용 박스(PrincipalDetails)에 담아서 리턴   
+        return new PrincipalDetails(user);
+    }
+}
