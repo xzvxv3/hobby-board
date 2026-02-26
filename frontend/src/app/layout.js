62f0b804140395675c21989b4789077d33756d52
chadchan <112686545+coconutcococode@users.noreply.github.com>
2026-02-26 20:42:04 +0900

    [Refactor] 레이아웃 배경 수정

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -7,10 +7,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) { // 이 레이아웃을 사용하는 모든 페이지의 실제 콘텐츠가 들어갈 자리
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+      <html lang="ko" className="bg-zinc-50 dark:bg-zinc-950">
+        <body className="bg-zinc-50 dark:bg-zinc-950">
+            {children}
+        </body>
+      </html>
   );
 }
